6bc4b6901defaeeacc33b62a1dcf05879401a04a
Sampo Pyysalo <smp@is.s.u-tokyo.ac.jp>
2011-01-04 15:28:17 +0900

    Span sort order tweak

--- annotator.js
+++ annotator.js
@@ -452,16 +452,20 @@ var Annotator = function(containerElement, onStart) {
         if (tmp) {
           return tmp < 0 ? -1 : 1;
         }
-        // spans with more spans go last
+        // spans with more arcs go last
         var tmp = a.numArcs - b.numArcs;
         if (tmp) {
           return tmp < 0 ? -1 : 1;
         }
         // compare the span widths,
-        // put wider on bottom so they don't mess with arcs
+        // put wider on bottom so they don't mess with arcs, or shorter
+	// on bottom if there are no arcs.
         var ad = a.to - a.from;
         var bd = b.to - b.from;
         tmp = ad - bd;
+	if(a.numArcs == 0 && b.numArcs == 0) {
+	    tmp = -tmp;
+	} 
         if (tmp) {
           return tmp < 0 ? 1 : -1;
         }
