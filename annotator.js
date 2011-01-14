6cc51a30132c44538612758e2d97b70fb422b22e
Goran Topic <amadan@mad.scientist.com>
2011-01-14 13:35:20 +0900

    Uniform tower widths

--- annotator.js
+++ annotator.js
@@ -25,7 +25,7 @@ var Annotator = function(containerElement, onStart) {
   var margin = { x: 2, y: 1 };
   // fine tuning specifically for box text margins
   var boxTextMargin = { x: 0, y: 0 };
-  var space = 5;
+  var space = 4;
   var boxSpacing = 1;
   var curlyHeight = 6;
   var lineSpacing = 5;
@@ -512,7 +512,9 @@ var Annotator = function(containerElement, onStart) {
       lastSpan = span;
     }); // sortedSpans
 
-    var curliedTowers = {};
+    var spanAnnTexts = {};
+    data.spanAnnTexts = [];
+    data.towers = {};
     // sort spans in chunks for drawing purposes
     $.each(data.chunks, function(chunkNo, chunk) {
       chunk.spans.sort(function(a, b) {
@@ -545,10 +547,11 @@ var Annotator = function(containerElement, onStart) {
       $.each(chunk.spans, function(spanNo, span) {
         span.chunk = chunk;
         span.text = chunk.text.substring(span.from, span.to);
-        if (!curliedTowers[span.towerId]) {
-          curliedTowers[span.towerId] = true;
+        if (!data.towers[span.towerId]) {
+          data.towers[span.towerId] = [];
           span.drawCurly = true;
         }
+        data.towers[span.towerId].push(span);
 
 	// Find the most appropriate abbreviation according to text
         // width
@@ -561,6 +564,11 @@ var Annotator = function(containerElement, onStart) {
           span.abbrevText = annotationAbbreviations[span.type][abbrevIdx];
           abbrevIdx++;
 	}
+
+        if (!spanAnnTexts[span.abbrevText]) {
+          spanAnnTexts[span.abbrevText] = true;
+          data.spanAnnTexts.push(span.abbrevText);
+        }
       }); // chunk.spans
     }); // chunks
 
@@ -574,6 +582,8 @@ var Annotator = function(containerElement, onStart) {
       lastSpan = span;
     });
   }
+
+  // TODO do the towerId and lineIndex overlap?
 
   var placeReservation = function(span, box, reservations) {
     var newSlot = {
@@ -696,6 +706,27 @@ var Annotator = function(containerElement, onStart) {
     var text = svg.text(textGroup, 0, 0, textSpans, {'class': 'text'});
     var textHeight = text.getBBox().height;
 
+    // measure annotations
+    var dummySpan = svg.group({ 'class': 'span' });
+    var spanAnnBoxes = {};
+    $.each(data.spanAnnTexts, function(textNo, text) {
+      var spanText = svg.text(dummySpan, 0, 0, text);
+      spanAnnBoxes[text] = spanText.getBBox();
+    }); // data.spanAnnTexts
+    svg.remove(dummySpan);
+
+    // find biggest annotation in each tower
+    $.each(data.towers, function(towerNo, tower) {
+      var biggestBox = { width: 0 };
+      $.each(tower, function(spanNo, span) {
+        var annBox = spanAnnBoxes[span.abbrevText];
+        if (annBox.width > biggestBox.width) biggestBox = annBox;
+      }); // tower
+      $.each(tower, function(spanNo, span) {
+        span.annBox = biggestBox;
+      }); // tower
+    }); // data.towers
+
     var current = { x: margin.x + sentNumMargin, y: margin.y }; // TODO: we don't need some of this?
     var rows = [];
     var spanHeights = [];
@@ -727,11 +758,13 @@ var Annotator = function(containerElement, onStart) {
         });
 
         // measure the text span
-        var measureText = svg.text(textGroup, 0, 0,
-          chunk.text.substr(0, span.from - chunk.from));
-        var xFrom = measureText.getBBox().width;
-        if (xFrom < 0) xFrom = 0;
-        svg.remove(measureText);
+        var xFrom = 0;
+        if (span.from != chunk.from) {
+          var measureText = svg.text(textGroup, 0, 0,
+            chunk.text.substr(0, span.from - chunk.from));
+          xFrom = measureText.getBBox().width;
+          svg.remove(measureText);
+        }
         measureText = svg.text(textGroup, 0, 0,
           chunk.text.substr(0, span.to - chunk.from));
         var measureBox = measureText.getBBox();
@@ -746,19 +779,20 @@ var Annotator = function(containerElement, onStart) {
         svg.remove(measureText);
         var x = (xFrom + xTo) / 2;
 
-        var spanText = svg.text(span.group, x, y, span.abbrevText);
-        var spanBox = spanText.getBBox();
+        var spanBox = span.annBox;
+        var xx = spanBox.x + x;
+        var yy = spanBox.y + y;
+        var hh = spanBox.height;
 
 	// text margin fine-tuning
-	spanBox.y      += boxTextMargin.y;
-	spanBox.height -= 2*boxTextMargin.y;
+	yy += boxTextMargin.y;
+	hh -= 2*boxTextMargin.y;
 
-        svg.remove(spanText);
         span.rect = svg.rect(span.group,
-          spanBox.x - margin.x,
-          spanBox.y - margin.y,
+          xx - margin.x,
+          yy - margin.y,
           spanBox.width + 2 * margin.x,
-          spanBox.height + 2 * margin.y, {
+          hh + 2 * margin.y, {
             'class': 'span_' + span.type + ' span_default',
             rx: margin.x,
             ry: margin.y,
@@ -769,26 +803,25 @@ var Annotator = function(containerElement, onStart) {
 
         var yAdjust = placeReservation(span, rectBox, reservations);
         // this is monotonous due to sort:
-        span.height = yAdjust + spanBox.height + 3 * margin.y + curlyHeight + arcSpacing;
+        span.height = yAdjust + hh + 3 * margin.y + curlyHeight + arcSpacing;
         spanHeights[span.lineIndex * 2] = span.height;
-        $(span.rect).attr('y', spanBox.y - margin.y - yAdjust);
-        $(spanText).attr('y', y - yAdjust);
+        $(span.rect).attr('y', yy - margin.y - yAdjust);
         if (span.Negation) {
           svg.path(span.group, svg.createPath().
-              move(spanBox.x, spanBox.y - margin.y - yAdjust).
-              line(spanBox.x + spanBox.width,
-                spanBox.y + spanBox.height + margin.y - yAdjust),
+              move(xx, yy - margin.y - yAdjust).
+              line(xx + spanBox.width,
+                yy + hh + margin.y - yAdjust),
               { 'class': 'negation' });
           svg.path(span.group, svg.createPath().
-              move(spanBox.x + spanBox.width, spanBox.y - margin.y - yAdjust).
-              line(spanBox.x, spanBox.y + spanBox.height + margin.y - yAdjust),
+              move(xx + spanBox.width, yy - margin.y - yAdjust).
+              line(xx, yy + hh + margin.y - yAdjust),
               { 'class': 'negation' });
         }
-        svg.add(span.group, spanText);
+        var spanText = svg.text(span.group, x, y - yAdjust, span.abbrevText);
 
         // Make curlies to show the span
         if (span.drawCurly) {
-          var bottom = spanBox.y + spanBox.height + margin.y - yAdjust;
+          var bottom = yy + hh + margin.y - yAdjust;
           svg.path(span.group, svg.createPath()
               .move(xFrom, bottom + curlyHeight)
               .curveC(xFrom, bottom,
@@ -1460,7 +1493,7 @@ $(function() {
           return false;
         },
         success: function(response) {
-          console.log(response);
+          displayMessage(response);
           annotator.setUser(user, password);
           $('#auth_button').val('Logout');
           $('#auth_user').val('');
