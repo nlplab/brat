d063f61d5cc0b7d30f7de3372ef49cda7edf3eec
Goran Topic <amadan@mad.scientist.com>
2011-01-18 23:58:10 +0900

    fixed None type on unarc bug

--- annotator.js
+++ annotator.js
@@ -257,6 +257,7 @@ var Annotator = function(containerElement, onStart) {
         action: 'arc',
         origin: originSpanId,
         target: targetSpanId,
+        type: type,
       };
       $('#arc_origin').text(originSpan.type+' ("'+data.text.substring(originSpan.from, originSpan.to)+'")');
       $('#arc_target').text(targetSpan.type+' ("'+data.text.substring(targetSpan.from, targetSpan.to)+'")');
