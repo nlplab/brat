32921464fd9a1331b8089eaf37191dcf4ce1d200
Pontus Stenetorp <pontus@stenetorp.se>
2011-01-20 16:54:09 +0900

    Some comments and conformity changes, nothing functional.

--- annspec.py
+++ annspec.py
@@ -1,12 +1,16 @@
 '''
-Configuration parameters for ${WITTY_NAME_OF_SOFTWARE}
+Annotation specification file, used to determine possible choices and for
+verifying the correctness by the verifier ${WITTY_NAME_OF_SOFTWARE}
 
 Author:     Sampo Pyysalo
-Version:    2011-01-19
+Version:    2011-01-20
 '''
 
-# Configuration for annotation types and semantics
+#TODO: These are really constants, upper-case the names, remember to make
+# the other files that import them conform too.
+#TODO: Silly, but : placement isn't really standard.
 
+## Configuration for annotation types and semantics
 # Types of textbounds representing physical entities.
 physical_entity_types = [
     'Protein',
@@ -18,64 +22,64 @@ physical_entity_types = [
 
 # abbrevs
 theme_only_argument = {
-        "Theme" : ["Protein"],
+        'Theme' : ['Protein'],
         }
 
 theme_and_site_arguments = {
-        "Theme" : ["Protein"],
-        "Site"  : ["Entity"],
+        'Theme' : ['Protein'],
+        'Site'  : ['Entity'],
         }
 
 regulation_arguments = {
-        "Theme" : ["Protein", "event"],
-        "Cause" : ["Protein", "event"],
-        "Site"  : ["Entity"],
-        "CSite" : ["Entity"],
+        'Theme' : ['Protein', 'event'],
+        'Cause' : ['Protein', 'event'],
+        'Site'  : ['Entity'],
+        'CSite' : ['Entity'],
         }
 
 localization_arguments = {
-        "Theme" : ["Protein"],
-        "AtLoc" : ["Entity"],
-        "ToLoc" : ["Entity"],
+        'Theme' : ['Protein'],
+        'AtLoc' : ['Entity'],
+        'ToLoc' : ['Entity'],
         }
 
 sidechain_modification_arguments = {
-        "Theme"     : ["Protein"],
-        "Site"      : ["Entity"],
-        "Sidechain" : ["Entity"],
+        'Theme'     : ['Protein'],
+        'Site'      : ['Entity'],
+        'Sidechain' : ['Entity'],
         }
 
 contextgene_modification_arguments = {
-    "Theme"       : ["Protein"],
-    "Site"        : ["Entity"],
-    "Contextgene" : ["Protein"],
+    'Theme'       : ['Protein'],
+    'Site'        : ['Entity'],
+    'Contextgene' : ['Protein'],
     }
 
 event_argument_types = {
     # GENIA
-    "default"             : theme_only_argument,
-    "Phosphorylation"     : theme_and_site_arguments,
-    "Localization"        : localization_arguments,
-    "Binding"             : theme_and_site_arguments,
-    "Regulation"          : regulation_arguments,
-    "Positive_regulation" : regulation_arguments,
-    "Negative_regulation" : regulation_arguments,
+    'default'             : theme_only_argument,
+    'Phosphorylation'     : theme_and_site_arguments,
+    'Localization'        : localization_arguments,
+    'Binding'             : theme_and_site_arguments,
+    'Regulation'          : regulation_arguments,
+    'Positive_regulation' : regulation_arguments,
+    'Negative_regulation' : regulation_arguments,
 
     # EPI
-    "Dephosphorylation"   : theme_and_site_arguments,
-    "Hydroxylation"       : theme_and_site_arguments,
-    "Dehydroxylation"     : theme_and_site_arguments,
-    "Ubiquitination"      : theme_and_site_arguments,
-    "Deubiquitination"    : theme_and_site_arguments,
-    "DNA_methylation"     : theme_and_site_arguments,
-    "DNA_demethylation"   : theme_and_site_arguments,
-    "Glycosylation"       : sidechain_modification_arguments,
-    "Deglycosylation"     : sidechain_modification_arguments,
-    "Acetylation"         : contextgene_modification_arguments,
-    "Deacetylation"       : contextgene_modification_arguments,
-    "Methylation"         : contextgene_modification_arguments,
-    "Demethylation"       : contextgene_modification_arguments,
-    "Catalysis"           : regulation_arguments,
+    'Dephosphorylation'   : theme_and_site_arguments,
+    'Hydroxylation'       : theme_and_site_arguments,
+    'Dehydroxylation'     : theme_and_site_arguments,
+    'Ubiquitination'      : theme_and_site_arguments,
+    'Deubiquitination'    : theme_and_site_arguments,
+    'DNA_methylation'     : theme_and_site_arguments,
+    'DNA_demethylation'   : theme_and_site_arguments,
+    'Glycosylation'       : sidechain_modification_arguments,
+    'Deglycosylation'     : sidechain_modification_arguments,
+    'Acetylation'         : contextgene_modification_arguments,
+    'Deacetylation'       : contextgene_modification_arguments,
+    'Methylation'         : contextgene_modification_arguments,
+    'Demethylation'       : contextgene_modification_arguments,
+    'Catalysis'           : regulation_arguments,
 
     # TODO: ID
     }
