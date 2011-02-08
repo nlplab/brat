29c09493b79571f335b7812d1daf3a85577996fd
Goran Topic <amadan@mad.scientist.com>
2011-02-08 16:32:01 +0900

    Fixes problems with lowercase span IDs

--- annotator.js
+++ annotator.js
@@ -1798,6 +1798,10 @@ $(function() {
     var saveUser;
     var savePassword;
 
+    var normalize = function(str) {
+      return str.toLowerCase().replace(' ', '_');
+    }
+
     var URLHash = function() {
       var original = window.location.hash;
       var parts = original.substr(1).split('/');
@@ -1995,9 +1999,13 @@ $(function() {
       if (span) {
         $('#span_highlight_link').css('display', 'inline').attr('href', document.location + '/' + span.id);
         annotator.keymap[46] = 'del_span_button'; // Del
-        var el = $('#span_' + span.type);
+        var el = $('#span_' + normalize(span.type));
         if (el.length) {
           el[0].checked = true;
+        } else {
+          $('#span_form input:radio:checked').each(function (radioNo, radio) {
+              radio.checked = false;
+          });
         }
       } else {
         $('#span_highlight_link').css('display', 'none');
@@ -2027,6 +2035,9 @@ $(function() {
       var type = $('#arc_form input:radio:checked').val();
       if (type) { // (if not cancelled)
         arcSubmit(type);
+      } else {
+        displayMessage('Error: No type selected', true);
+        hideAllForms();
       }
       return false;
     };
@@ -2055,7 +2066,7 @@ $(function() {
                 annotator.keymap = jsonData.keymap;
                 if (arcId) {
                   $('#arc_highlight_link').css('display', 'inline').attr('href', document.location + '/' + arcId);
-                  var el = $('#arc_' + arcType)[0];
+                  var el = $('#arc_' + normalize(arcType))[0];
                   if (el) {
                     el.checked = true;
                   }
@@ -2220,7 +2231,7 @@ $(function() {
     spanForm.css('display', 'none');
     annotator.keymap = {};
     var type = $('#span_form input:radio:checked').val();
-    if (type) { // (if not cancelled)
+    if (type) {
       annotator.ajaxOptions.type = type;
       var el;
       if (el = $('#span_mod_negation')[0]) {
@@ -2230,6 +2241,9 @@ $(function() {
         annotator.ajaxOptions.speculation = el.checked;
       }
       annotator.postChangesAndReload();
+    } else {
+      hideAllForms();
+      displayMessage('Error: No type selected', true);
     }
     return false;
   };
