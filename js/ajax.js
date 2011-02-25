e0b0a5e037a89378da43d8fab3bea882439a01a2
Goran Topic <amadan@mad.scientist.com>
2011-02-25 17:13:37 +0900

    added some missing files

---
+++ js/ajax.js
@@ -0,0 +1,27 @@
+var Ajax = (function($, window, undefined) {
+    var Ajax = function(dispatcher) {
+      var ajax = this;
+
+      var ajaxCall = function(data, callback) {
+        $.ajax({
+          url: 'ajax.cgi',
+          data: data,
+          method: 'POST',
+          success: function(response) {
+            dispatcher.post("messages", response.messages);
+            if (!response.error && callback) {
+              dispatcher.post(0, callback, response);
+            }
+          },
+          error: function(x) {
+            console.error(x);
+          }
+        });
+      };
+
+      dispatcher.
+          on("ajax", ajaxCall);
+    };
+
+    return Ajax;
+})(jQuery, window);
