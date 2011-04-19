09b6a68e8e58cf00460be724299c67f822b196d0
Goran Topic <amadan@mad.scientist.com>
2011-04-19 14:05:38 +0900

    Visualizer-only branch, initial commit

--- js/annotator.js
+++
@@ -1,8 +0,0 @@
-var Annotator = (function($, window, undefined) {
-    var Annotator = function(dispatcher, visualizer, svg) {
-      var that = this;
-
-    };
-    
-    return Annotator;
-})(jQuery, window);
