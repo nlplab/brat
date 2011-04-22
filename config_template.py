b557fd719414178b5ebc5038ab40b9064bbf1f89
sampo <smp@Kirin.(none)>
2011-04-22 17:58:23 +0900

    Trim config template of parts not related to visualization.

--- config_template.py
+++ config_template.py
@@ -11,26 +11,17 @@ Version:    2011-01-19
 
 BASE_DIR = CHANGE_ME
 DATA_DIR = CHANGE_ME
-BACKUP_DIR = CHANGE_ME
 
 # Below are commented out common configurations for BASE_DIR and DATA_DIR
 '''
 from os.path import dirname, join
 
 BASE_DIR = dirname(__file__)
-DATA_DIR = join(BASE_DIR, RELATIVE_PATH_TO_DATA_DIR)
-BACKUP_DIR = join(BASE_DIR, RELATIVE_PATH_TO_BACK_UP_DIR)
+DATA_DIR = join(BASE_DIR, 'data')
 '''
-
-# TODO: Remove these when we have a back-end
-USER_PASSWORD = {
-    CHANGE_ME : CHANGE_ME
-    }
 
 # Enable additional debug output
 DEBUG = False
 
 # Contact email for users to use if the software encounters errors
 ADMIN_CONTACT_EMAIL = CHANGE_ME
-
-assert DATA_DIR != BACKUP_DIR
