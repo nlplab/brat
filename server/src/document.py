011a691e7fb16ccf4bb642257820327ccfda3d28
sampo <smp@Kirin.(none)>
2011-05-06 11:10:59 +0900

    More explanatory failure message from geniass not running.

--- server/src/document.py
+++ server/src/document.py
@@ -221,7 +221,10 @@ def _sentence_split(txt_file_path):
         # fake an exception and fall into the heuristic. This happens for
         # linking errors among other things.
         if not ret:
-            display_message("Warning: sentence split failed (geniass not set up?)", type='warning')
+            # NOTE: this also happens for missing write permissions, as
+            # geniass assumes it can write a temporary into the directory
+            # where the .txt is found.
+            display_message("Warning: sentence split failed (geniass not set up, or no write permission to directory?)", type='warning')
             err = OSError()
             err.errno = 2
             raise err
