nlplab/brat

commit 9fd19aa165b0aaac55e416cc919463f927120995

Fix bug where arc type hot key would always select currently selected radio button instead of that for the hotkey.

client/src/annotator_ui.js
@@ -174,8 +174,9 @@ var AnnotatorUI = (function($, window, undefined) {
         adjustToCursor(evt, spanForm.parent());
       };
 
-      var arcFormSubmit = function(evt) {
-        var type = $('#arc_form input:radio:checked').val();
+      var arcFormSubmit = function(evt) {	
+	var typeRadio = $(evt.target) || $('#arc_form input:radio:checked');
+        var type = typeRadio.val();
         dispatcher.post('hideForm', [arcForm]);
 
         if (type) { // (if not cancelled)

commit 102daa2c4a2f2a81cbf5aacc1be948722bb9f74f

Fix bug introduced in 9fd19aa165b0aaac55e4

client/src/annotator_ui.js
@@ -174,8 +174,13 @@ var AnnotatorUI = (function($, window, undefined) {
         adjustToCursor(evt, spanForm.parent());
       };
 
-      var arcFormSubmit = function(evt) {	
-	var typeRadio = $(evt.target) || $('#arc_form input:radio:checked');
+      var arcFormSubmitRadio = function(evt) {
+	  // TODO: check for confirm_mode?
+          arcFormSubmit(evt, $(evt.target));
+      }
+
+      var arcFormSubmit = function(evt, typeRadio) {	
+	typeRadio = typeRadio || $('#arc_form input:radio:checked');
         var type = typeRadio.val();
         dispatcher.post('hideForm', [arcForm]);
 
@@ -217,7 +222,7 @@ var AnnotatorUI = (function($, window, undefined) {
               }
               var confirmMode = $('#confirm_mode')[0].checked;
               if (!confirmMode) {
-                arcForm.find('#arc_roles input:radio').click(arcFormSubmit);
+                arcForm.find('#arc_roles input:radio').click(arcFormSubmitRadio);
               }
               if (arcType) {
                 $('#arc_form_delete').show();