f000b38e94b1372709b8ade837b58e84ea70e3b9
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2011-09-21 13:42:19 +0900

    don't crash when looking up arrowHead for undefined arcDesc

--- client/src/visualizer.js
+++ client/src/visualizer.js
@@ -1503,7 +1503,7 @@ Util.profileStart('arcs');
               path.line(from, -height);
             }
             var hashlessColor = color.replace('#', '');
-            var arrowType = arrows[(leftToRight ? arcDesc.arrowTail : arcDesc.arrowHead) + ',' + hashlessColor];
+            var arrowType = arcDesc && arcDesc.arrowHead ? (arrows[(leftToRight ? arcDesc.arrowTail : arcDesc.arrowHead) + ',' + hashlessColor]) : 'triangle,5,' +  hashlessColor;
             svg.path(arcGroup, path, {
               markerEnd: arrowType && ('url(#' + arrowType + ')'),
               style: 'stroke: ' + color,
@@ -1545,7 +1545,7 @@ Util.profileStart('arcs');
             } else {
               path.line(to, -height);
             }
-            var arrowType = arrows[(leftToRight ? arcDesc.arrowHead : arcDesc.arrowTail) + ',' + hashlessColor];
+            var arrowType = arcDesc && arcDesc.arrowTail ? (arrows[(leftToRight ? arcDesc.arrowHead : arcDesc.arrowTail) + ',' + hashlessColor]) : 'triangle,5,' +  hashlessColor;
             svg.path(arcGroup, path, {
                 markerEnd: arrowType && ('url(#' + arrowType + ')'),
                 style: 'stroke: ' + color,
