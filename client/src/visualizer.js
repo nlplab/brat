bb4c568d4a698e6d43ec531701a3827959f3ff5b
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2011-09-29 16:18:21 +0900

    One more bug in overlapping span draw sort algo.

--- client/src/visualizer.js
+++ client/src/visualizer.js
@@ -1680,7 +1680,7 @@ Util.profileStart('chunkFinish');
         var rlChunkComp = function(a,b) { 
           var ac = currentChunk.spans[a];
           var bc = currentChunk.spans[b]
-          var endDiff = Util.cmp(ac.from, bc.from);
+          var endDiff = Util.cmp(bc.to, ac.to);
           return endDiff != 0 ? endDiff : Util.cmp(bc.to-bc.from, ac.to-ac.from);
         }
 
