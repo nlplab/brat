f96acbd142e08fac787e677eb670dad511fae2e0
Pontus Stenetorp <pontus@stenetorp.se>
2011-09-30 20:01:44 +0900

    New agnostic server API.

--- server/src/server.py
+++ server/src/server.py
@@ -9,53 +9,137 @@ processes potential exceptions before returning them to be sent as responses.
 NOTE(S):
 
 * Defer imports until failures can be catched
+* Stay compatible with Python 2.3 until we verify the Python version
 
 Author:     Pontus  Stenetorp   <pontus is s u tokyo ac jp>
 Version:    2010-09-29
 '''
 
+# Standard library version
+from os.path import abspath
+from os.path import join as path_join
+from sys import version_info, stderr
+from time import time
 
-class InstallationIntegrityError(Exception):
+### Constants
+# This handling of version_info is strictly for backwards compability
+PY_VER_STR = '%d.%d.%d-%s-%d' % tuple(version_info)
+REQUIRED_PY_VERSION_MAJOR = 2
+REQUIRED_PY_VERSION_MINOR = 5
+JSON_HDR = ('Content-Type', 'application/json')
+CONF_FNAME = 'config.py'
+CONF_TEMPLATE_FNAME = 'config_template.py'
+###
+
+
+class PermissionError(Exception):
     pass
 
 
-# TODO: Ensure installation integrity
-def _integrity_check():
-    pass
+# TODO: Possibly check configurations too
+# TODO: Extend to check __everything__?
+def _permission_check():
+    from os import access, R_OK, W_OK
+    from config import DATA_DIR, WORK_DIR
+    from jsonwrap import dumps
+    from message import Messager
+
+    if not access(WORK_DIR, R_OK | W_OK):
+        Messager.error((('Work dir: "%s" is not read-able and ' % WORK_DIR) +
+                'write-able by the server'), duration=-1)
+        raise PermissionError
+    
+    if not access(DATA_DIR, R_OK):
+        Messager.error((('Data dir: "%s" is not read-able ' % DATA_DIR) +
+                'by the server'), duration=-1)
+        raise PermissionError
 
 
 class ConfigurationError(Exception):
     pass
 
 
-# TODO: Ensure installation configuration
+# Error message template functions
+def _miss_var_msg(var):
+    return ('Missing variable "%s" in %s, make sure that you have '
+            'not made any errors to your configurations and to start over '
+            'copy the template file %s to %s in your '
+            'installation directory and edit it to suit your environment'
+            ) % (var, CONF_FNAME, CONF_TEMPLATE_FNAME, CONF_FNAME)
+
+def _miss_config_msg():
+    return ('Missing file %s in the installation dir. If this is a new '
+            'installation, copy the template file %s to %s in '
+            'your installation directory ("cp %s %s") and edit '
+            'it to suit your environment.'
+            ) % (CONF_FNAME, CONF_TEMPLATE_FNAME, CONF_FNAME, CONF_FNAME,
+                CONF_TEMPLATE_FNAME)
+
+# Check for existance and sanity of the configuration
 def _config_check():
-    pass
+    from message import Messager
+    
+    from sys import path
+    from copy import deepcopy
+    from os.path import dirname
+    # Reset the path to force config.py to be in the root (could be hacked
+    #       using __init__.py, but we can be monkey-patched anyway)
+    orig_path = deepcopy(path)
+    # Can't you empty in O(1) instead of O(N)?
+    while path:
+        path.pop()
+    path.append(path_join(abspath(dirname(__file__)), '../..'))
+    # Check if we have a config, otherwise whine
+    try:
+        import config
+        del config
+    except ImportError:
+        path.extend(orig_path)
+        Messager.error(_miss_config_msg(), duration=-1)
+        raise ConfigurationError
+    # Try importing the config entries we need
+    try:
+        from config import DEBUG
+    except ImportError:
+        path.extend(orig_path)
+        Messager.error(_miss_var_msg('DEBUG'), duration=-1)
+        raise ConfigurationError
+    try:
+        from config import ADMIN_CONTACT_EMAIL
+    except ImportError:
+        path.extend(orig_path)
+        Messager.error(_miss_var_msg('ADMIN_CONTACT_EMAIL'), duration=-1)
+        raise ConfigurationError
+    # Remove our entry to the path
+    path.pop()
+    # Then restore it
+    path.extend(orig_path)
 
-# TODO: Returns json data or None!
-def serve(params, client_ip, client_hostname):
-    # At this stage we can not get any cookie data, wait-for-it
-    cookie_hdrs = None
-
-    # TODO: SANITY!
-    # TODO: Enable logging!
-
-    # We can now get the cookie
+def _safe_serve(params, client_ip, client_hostname):
+    from common import ProtocolError, NoPrintJSONError
+    from config import WORK_DIR
+    from dispatch import dispatch
+    from jsonwrap import dumps
+    from logging import basicConfig as log_basic_config
+    from message import Messager
     from session import get_session
+
+    # Enable logging
+    try:
+        from config import LOG_LEVEL
+    except ImportError:
+        from logging import WARNING as LOG_LEVEL_WARNING
+        LOG_LEVEL = LOG_LEVEL_WARNING
+    log_basic_config(filename=path_join(WORK_DIR, 'server.log'),
+            level=LOG_LEVEL)
+
+    # Session information is now available
     cookie_hdrs = get_session().get_cookie_hdrs()
 
-    ### TODO: Move around these imports
-    from common import ProtocolError, NoPrintJSONError
-    from jsonwrap import dumps
-    from message import Messager
-    ###
-
-    from dispatch import dispatch
     try:
+        # Dispatch the request
         json_dic = dispatch(params, client_ip, client_hostname)
-
-        response_data = (('Content-Type', 'application/json'),
-                dumps(Messager.output_json(json_dic)))
+        response_data = ((JSON_HDR, ), dumps(Messager.output_json(json_dic)))
     except ProtocolError, e:
         # Internal error, only reported to client not to log
         json_dic = {}
@@ -63,19 +147,98 @@ def serve(params, client_ip, client_hostname):
 
         # Add a human-readable version of the error
         err_str = str(e)
-        if not err_str:
-            Messager.error(e)
-        
-        response_data = (('Content-Type', 'application/json'),
-                dumps(Messager.output_json(json_dic)))
-    except NoPrintJSONError, e:
-        # TODO: Re-wire so that the error holds the content
-        assert False, 'Not implemented!'
+        if err_str != '':
+            Messager.error(err_str)
 
-    # TODO: Should the cookie always go back even upon errors?
+        response_data = ((JSON_HDR, ), dumps(Messager.output_json(json_dic)))
+    except NoPrintJSONError, e:
+        # Terrible hack to serve other things than JSON
+        response_data = (e.hdrs, e.data)
+
     return (cookie_hdrs, response_data)
 
-# TODO: Small test
-if __name__ == '__main__':
-    from sys import argv, exit
-    exit(main(argv))
+# Encapsulate an interpreter crash
+def _server_crash(cookie_hdrs, e):
+    from config import ADMIN_CONTACT_EMAIL, DEBUG
+    from jsonwrap import dumps
+    from message import Messager
+    from traceback import print_exc
+    
+    try:
+        from cStringIO import StringIO
+    except ImportError:
+        from StringIO import StringIO
+
+    # Getting the stack-trace requires a small trick
+    buf = StringIO()
+    print_exc(file=buf)
+    buf.seek(0)
+    stack_trace = buf.read()
+
+    if DEBUG:
+        # Send back the stack-trace as json
+        error_msg = '\n'.join(('Server Python crash, stack-trace is:\n',
+            stack_trace))
+        Messager.error(error_msg, duration=-1)
+    else:
+        # Give the user an error message
+        # Use the current time since epoch as an id for later log look-up
+        error_msg = ('The server encountered a serious error, '
+                'please contact the administrators at %s '
+                'and give the id #%d'
+                ) % (ADMIN_CONTACT_EMAIL, int(time()))
+        Messager.error(error_msg, duration=-1)
+
+    # Print to stderr so that the exception is logged by the webserver
+    print >> stderr, stack_trace
+
+    json_dic = {
+            'exception': True,
+            }
+    return (cookie_hdrs, ((JSON_HDR, ), dumps(Messager.output_json(json_dic))))
+
+# Serve the client request
+def serve(params, client_ip, client_hostname):
+    # At this stage we can not get any cookie data, wait-for-it
+    cookie_hdrs = None
+
+    # Do we have a Python version compatibly with our libs?
+    if (version_info[0] != REQUIRED_PY_VERSION_MAJOR or
+            version_info[1] < REQUIRED_PY_VERSION_MINOR):
+        # Bail with hand-writen JSON, this is very fragile to protocol changes
+        return cookie_hdrs, ((JSON_HDR, ),
+                ('''
+{
+  "messages": [
+    [
+      "Incompatible Python version (%s), %d.%d or above is supported",
+      "error",
+      -1
+    ]
+  ]
+}
+                ''' % (PY_VER_STR, REQUIRED_PY_VERSION_MAJOR,
+                    REQUIRED_PY_VERSION_MINOR)).strip())
+
+    # We can now safely use json and Messager
+    from jsonwrap import dumps
+    from message import Messager
+    
+    try:
+        _config_check()
+    except ConfigurationError:
+        return cookie_hdrs, ((JSON_HDR, ), dumps(Messager.output_json({})))
+    # We can now safely read the config
+    from config import DEBUG
+
+    try:
+        _permission_check()
+    except PermissionError:
+        return cookie_hdrs, ((JSON_HDR, ), dumps(Messager.output_json({})))
+
+    try:
+        # Safe region, can throw any exception, has verified installation
+        return _safe_serve(params, client_ip, client_hostname)
+    except BaseException, e:
+        # Handle the server crash
+        return _server_crash(cookie_hdrs, e)
