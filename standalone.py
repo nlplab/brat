49ad60a5cd99873aa1c375855a7d5d7de2f80db7
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2012-10-22 00:34:39 +0900

    Direct invocation of server.serve, bypassing exec

--- standalone.py
+++ standalone.py
@@ -8,6 +8,7 @@
 import sys
 import os
 
+from cgi import FieldStorage
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 from SocketServer import ForkingMixIn
@@ -24,19 +25,60 @@ class BratHTTPRequestHandler(CGIHTTPRequestHandler):
     """Minimal handler for brat server."""
 
     def is_brat(self):
-        print self.path
         if self.path == '/ajax.cgi':
             return True
         else:
             return False    
 
-    def run_brat(self):
-        """Execute brat server."""
+    def run_brat_direct(self):
+        """Execute brat server directly."""
+
+        # following ajax.cgi
+
+        remote_addr = self.client_address[0]
+        remote_host = self.address_string()
+        cookie_data = ', '.join(filter(None, self.headers.getheaders('cookie')))
+
+        saved = sys.stdin, sys.stdout, sys.stderr
+        sys.stdin, sys.stdout = self.rfile, self.wfile
+
+        # set env to get FieldStorage to read params from STDIN
+        env = {}
+        env['REQUEST_METHOD'] = 'POST'
+        env['CONTENT_LENGTH'] = self.headers.getheader('content-length')
+        os.environ.update(env)
+        params = FieldStorage()
+
+        # Call main server
+        cookie_hdrs, response_data = serve(params, remote_addr, remote_host,
+                                           cookie_data)
+
+        sys.stdin, sys.stdout, sys.stderr = saved
+
+        # Package and send response
+        if cookie_hdrs is not None:
+            response_hdrs = [hdr for hdr in cookie_hdrs]
+        else:
+            response_hdrs = []
+        response_hdrs.extend(response_data[0])
+
+        self.send_response(200)
+        self.wfile.write('\n'.join('%s: %s' % (k, v) for k, v in response_hdrs))
+        self.wfile.write('\n')
+        self.wfile.write('\n')
+        # Hack to support binary data and general Unicode for SVGs and JSON
+        if isinstance(response_data[1], unicode):
+            self.wfile.write(response_data[1].encode('utf-8'))
+        else:
+            self.wfile.write(response_data[1])
+        return 0
+
+    def run_brat_exec(self):
+        """Execute brat server using execfile('ajax.cgi')."""
 
         # stipped down from CGIHTTPRequestHandler run_cgi()
 
-        scriptname = '/ajax.cgi'
-        scriptfile = self.translate_path(scriptname)
+        scriptfile = self.translate_path('/ajax.cgi')
 
         env = {}
         env['REQUEST_METHOD'] = 'POST'
@@ -66,7 +108,7 @@ class BratHTTPRequestHandler(CGIHTTPRequestHandler):
         """Serve a POST request. Only implemented for brat server."""
 
         if self.is_brat():
-            self.run_brat()
+            self.run_brat_direct()
         else:
             self.send_error(501, "Can only POST to brat")
 
