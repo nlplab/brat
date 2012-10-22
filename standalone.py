99589449e8653a5da504aeb8290876006404866b
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2012-10-22 10:54:36 +0900

    Restrictions, from http://github.com/spyysalo/pyhttp/restrictedserver.py

--- standalone.py
+++ standalone.py
@@ -8,6 +8,9 @@
 import sys
 import os
 
+from posixpath import normpath
+from urllib import unquote
+
 from cgi import FieldStorage
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
@@ -56,8 +59,101 @@ import verify_annotations
 _SERVER_ADDR = ''
 _SERVER_PORT = 8001
 
+_PERMISSIONS = """
+Disallow: *.py
+Disallow: *.cgi
+Disallow: /.htaccess
+Disallow: *.py~  # no emacs backups
+Disallow: *.cgi~
+Disallow: /.htaccess~
+Allow: /
+"""
+
+class PermissionParseError(Exception):
+    def __init__(self, linenum, line, message=None):
+        self.linenum = linenum
+        self.line = line
+        self.message = ' (%s)' % message if message is not None else ''
+    
+    def __str__(self):
+        return 'line %d%s: %s' % (self.linenum, self.message, self.line)
+
+class PathPattern(object):
+    def __init__(self, path):
+        self.path = path
+        self.plen = len(path)
+
+    def match(self, s):
+        # Require prefix match and separator/end.
+        return s[:self.plen] == self.path and (self.path[-1] == '/' or
+                                               s[self.plen:] == '' or 
+                                               s[self.plen] == '/')
+
+class ExtensionPattern(object):
+    def __init__(self, ext):
+        self.ext = ext
+
+    def match(self, s):
+        return os.path.splitext(s)[1] == self.ext
+
+class PathPermissions(object):
+    """Implements path permission checking with a robots.txt-like syntax."""
+
+    def __init__(self, default_allow=False):
+        self._entries = []
+        self.default_allow = default_allow
+
+    def allow(self, path):
+        # First match wins
+        for pattern, allow in self._entries:
+            if pattern.match(path):
+                return allow
+        return self.default_allow
+    
+    def parse(self, lines):
+        # Syntax: "DIRECTIVE : PATTERN" where
+        # DIRECTIVE is either "Disallow:" or "Allow:" and
+        # PATTERN either has the form "*.EXT" or "/PATH".
+        # Strings starting with "#" and empty lines are ignored.
+
+        for ln, l in enumerate(lines):            
+            i = l.find('#')
+            if i != -1:
+                l = l[:i]
+            l = l.strip()
+
+            if not l:
+                continue
+
+            i = l.find(':')
+            if i == -1:
+                raise PermissionParseError(ln, lines[ln], 'missing colon')
+
+            directive = l[:i].strip().lower()
+            pattern = l[i+1:].strip()
+
+            if directive == 'allow':
+                allow = True
+            elif directive == 'disallow':
+                allow = False
+            else:
+                raise PermissionParseError(ln, lines[ln], 'unrecognized directive')
+            
+            if pattern.startswith('/'):
+                patt = PathPattern(pattern)
+            elif pattern.startswith('*.'):
+                patt = ExtensionPattern(pattern[1:])
+            else:
+                raise PermissionParseError(ln, lines[ln], 'unrecognized pattern')
+
+            self._entries.append((patt, allow))
+
+        return self
+
 class BratHTTPRequestHandler(CGIHTTPRequestHandler):
     """Minimal handler for brat server."""
+
+    permissions = PathPermissions().parse(_PERMISSIONS.split('\n'))
 
     def is_brat(self):
         if self.path == '/ajax.cgi':
@@ -139,6 +235,28 @@ class BratHTTPRequestHandler(CGIHTTPRequestHandler):
         else:
             print >> sys.stderr, 'exit OK'
 
+    def allow_path(self):
+        """Test whether to allow a request for self.path."""
+
+        # Cleanup in part following SimpleHTTPServer.translate_path()
+        path = self.path
+        path = path.split('?', 1)[0]
+        path = path.split('#', 1)[0]
+        path = unquote(path)
+        path = normpath(path)
+        parts = path.split('/')
+        parts = filter(None, parts)
+        if '..' in parts:
+            return False
+        path = '/'+'/'.join(parts)
+
+        return self.permissions.allow(path)
+
+    def list_directory(self, path):
+        """Override SimpleHTTPRequestHandler.list_directory()"""
+        # TODO: permissions for directory listings
+        self.send_error(403)
+
     def do_POST(self):
         """Serve a POST request. Only implemented for brat server."""
 
@@ -146,10 +264,6 @@ class BratHTTPRequestHandler(CGIHTTPRequestHandler):
             self.run_brat_direct()
         else:
             self.send_error(501, "Can only POST to brat")
-
-    def allow_path(self):
-        # TODO
-        return True
 
     def do_GET(self):
         """Serve a GET request."""
