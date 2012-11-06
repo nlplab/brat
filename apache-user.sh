5efe40beb07d287e8f18d5b632d2bc5158807bb7
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2012-11-06 21:19:29 +0900

    Don't match the script itself

--- apache-user.sh
+++ apache-user.sh
@@ -2,6 +2,6 @@
 
 # Script for determining the (likely) apache user
 
-apache_user=`ps aux | grep -v 'tomcat' | grep '[a]pache\|[h]ttpd' | cut -d ' ' -f 1 | grep -v '^root$' | head -n 1`
+apache_user=`ps aux | grep -v 'apache-user' | grep -v 'apache-group' | grep -v 'tomcat' | grep '[a]pache\|[h]ttpd' | cut -d ' ' -f 1 | grep -v '^root$' | head -n 1`
 
 echo $apache_user
