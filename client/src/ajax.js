95c65bbe5b8e65ac6517d271ac045f667327fe96
Goran Topic <amadan@mad.scientist.com>
2013-09-02 14:17:30 +0900

    Update AJAX to work with FileData object (JIC)

--- client/src/ajax.js
+++ client/src/ajax.js
@@ -1,6 +1,7 @@
 // -*- Mode: JavaScript; tab-width: 2; indent-tabs-mode: nil; -*-
 // vim:set ft=javascript ts=2 sw=2 sts=2 cindent:
 var Ajax = (function($, window, undefined) {
+    var PROTOCOL_VERSION = 1
     var Ajax = function(dispatcher) {
       var that = this;
       var pending = 0;
@@ -9,7 +10,7 @@ var Ajax = (function($, window, undefined) {
 
       // merge data will get merged into the response data
       // before calling the callback
-      var ajaxCall = function(data, callback, merge) {
+      var ajaxCall = function(data, callback, merge, extraOptions) {
         merge = merge || {};
         dispatcher.post('spin');
         pending++;
@@ -20,73 +21,80 @@ var Ajax = (function($, window, undefined) {
         delete merge.keep;
 
         // If no protocol version is explicitly set, set it to current
-        if (data['protocol'] === undefined) {
+        if (data.toString() == '[object FormData]') {
+          data.append('protocol', PROTOCOL_VERSION);
+        } else if (data['protocol'] === undefined) {
           // TODO: Extract the protocol version somewhere global
-          data['protocol'] = 1;
+          data['protocol'] = PROTOCOL_VERSION;
         }
 
-        $.ajax({
-          url: 'ajax.cgi',
-          data: data,
-          type: 'POST',
-          success: function(response) {
-            pending--;
-            // If no exception is set, verify the server results
-            if (response.exception == undefined && response.action !== data.action) {
-              console.error('Action ' + data.action +
-                ' returned the results of action ' + response.action);
-              response.exception = true;
-              dispatcher.post('messages', [[['Protocol error: Action' + data.action + ' returned the results of action ' + response.action + ' maybe the server is unable to run, please run tools/troubleshooting.sh from your installation to diagnose it', 'error', -1]]]);
-            }
+        options = {
+            url: 'ajax.cgi',
+            data: data,
+            type: 'POST',
+            success: function(response) {
+              pending--;
+              // If no exception is set, verify the server results
+              if (response.exception == undefined && response.action !== data.action) {
+                console.error('Action ' + data.action +
+                  ' returned the results of action ' + response.action);
+                response.exception = true;
+                dispatcher.post('messages', [[['Protocol error: Action' + data.action + ' returned the results of action ' + response.action + ' maybe the server is unable to run, please run tools/troubleshooting.sh from your installation to diagnose it', 'error', -1]]]);
+              }
 
-            // If the request is obsolete, do nothing; if not...
-            if (pendingList.hasOwnProperty(id)) {
-              dispatcher.post('messages', [response.messages]);
-              if (response.exception == 'configurationError'
-                  || response.exception == 'protocolVersionMismatch') {
-                // this is a no-rescue critical failure.
-                // Stop *everything*.
-                pendingList = {};
-                dispatcher.post('screamingHalt');
-                // If we had a protocol mismatch, prompt the user for a reload
-                if (response.exception == 'protocolVersionMismatch') {
-                  if(confirm('The server is running a different version ' +
-                      'from brat than your client, possibly due to a ' +
-                      'server upgrade. Would you like to reload the ' +
-                      'current page to update your client to the latest ' +
-                      'version?')) {
-                    window.location.reload(true);
-                  } else {
-                    dispatcher.post('messages', [[['Fatal Error: Protocol ' +
-                        'version mismatch, please contact the administrator',
-                        'error', -1]]]);
+              // If the request is obsolete, do nothing; if not...
+              if (pendingList.hasOwnProperty(id)) {
+                dispatcher.post('messages', [response.messages]);
+                if (response.exception == 'configurationError'
+                    || response.exception == 'protocolVersionMismatch') {
+                  // this is a no-rescue critical failure.
+                  // Stop *everything*.
+                  pendingList = {};
+                  dispatcher.post('screamingHalt');
+                  // If we had a protocol mismatch, prompt the user for a reload
+                  if (response.exception == 'protocolVersionMismatch') {
+                    if(confirm('The server is running a different version ' +
+                        'from brat than your client, possibly due to a ' +
+                        'server upgrade. Would you like to reload the ' +
+                        'current page to update your client to the latest ' +
+                        'version?')) {
+                      window.location.reload(true);
+                    } else {
+                      dispatcher.post('messages', [[['Fatal Error: Protocol ' +
+                          'version mismatch, please contact the administrator',
+                          'error', -1]]]);
+                    }
                   }
+                  return;
                 }
-                return;
-              }
 
-              delete pendingList[id];
+                delete pendingList[id];
 
-              // if .exception is just Boolean true, do not process
-              // the callback; if it is anything else, the
-              // callback is responsible for handling it
-              if (response.exception == true) {
-                $('#waiter').dialog('close');
-              } else if (callback) {
-                $.extend(response, merge);
-                dispatcher.post(0, callback, [response]);
+                // if .exception is just Boolean true, do not process
+                // the callback; if it is anything else, the
+                // callback is responsible for handling it
+                if (response.exception == true) {
+                  $('#waiter').dialog('close');
+                } else if (callback) {
+                  $.extend(response, merge);
+                  dispatcher.post(0, callback, [response]);
+                }
               }
+              dispatcher.post('unspin');
+            },
+            error: function(response, textStatus, errorThrown) {
+              pending--;
+              dispatcher.post('unspin');
+              $('#waiter').dialog('close');
+              dispatcher.post('messages', [[['Error: Action' + data.action + ' failed on error ' + response.statusText, 'error']]]);
+              console.error(textStatus + ':', errorThrown, response);
             }
-            dispatcher.post('unspin');
-          },
-          error: function(response, textStatus, errorThrown) {
-            pending--;
-            dispatcher.post('unspin');
-            $('#waiter').dialog('close');
-            dispatcher.post('messages', [[['Error: Action' + data.action + ' failed on error ' + response.statusText, 'error']]]);
-            console.error(textStatus + ':', errorThrown, response);
-          }
-        });
+          };
+
+        if (extraOptions) {
+          $.extend(options, extraOptions);
+        }
+        $.ajax(options);
         return id;
       };
 
