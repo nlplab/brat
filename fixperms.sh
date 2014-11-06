cda5712a7e416004c184d5d5812b7df7d3028733
Jesse Dunietz <yavyash@gmail.com>
2014-11-05 20:16:23 -0500

    Added location param to fixperms

--- fixperms.sh
+++ fixperms.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo find data -not -name .stats_cache -type f -exec chown $USER:www-data {} \; -exec chmod 660 {} \; # ug=rw, o=
-sudo find data -type d -exec sudo chown $USER:www-data {} \; -exec chmod 770 {} \; # ug=rwx, o=
-sudo find data -name .stats_cache -exec chown www-data:www-data {} \; -exec chmod 644 {} \; # u=rw, ro=r
+DATADIR=${1:-data} # Argument 1, defaulting to 'data' if not set
+
+sudo find $DATADIR -not -name .stats_cache -type f -exec chown $USER:www-data {} \; -exec chmod 660 {} \; # ug=rw, o=
+sudo find $DATADIR -type d -exec sudo chown $USER:www-data {} \; -exec chmod 770 {} \; # ug=rwx, o=
+sudo find $DATADIR -name .stats_cache -exec chown www-data:www-data {} \; -exec chmod 644 {} \; # u=rw, ro=r
