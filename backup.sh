948b89db1d11064ace07297c2db911b724641019
Gemma Hentsch <gemma@halfapenguin.com>
2019-11-04 15:30:37 +0000

    s3 backup variant

--- backup.sh
+++ backup.sh
@@ -3,8 +3,12 @@
 current_time=$(date "+%Y-%m-%d-%H-%M-%S")
 script_path="`dirname \"$0\"`"
 
-output_filename="$script_path/$current_time-data-backup.tar.gz"
+#output_filename="$script_path/$current_time-data-backup.tar.gz"
 
 
-echo "$script_path"/data/
-tar -cvzf $output_filename "$script_path"/data/
+
+#echo "$script_path"/data/
+#tar -cvzf $output_filename
+echo $script_path
+echo $current_time
+aws s3 cp "$script_path"/data/ s3://brat-data-backup/"$current_time"/ --recursive
